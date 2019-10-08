5d130e49ff8173f5764aad196367f305337bdac5
mark bashir <beejayphil@gmail.com>
2019-10-08 09:58:59 +0100

    solution to aladin carpet problem

--- utils/aladin.js
+++ utils/aladin.js
@@ -1,4 +1,20 @@
-function aladin() { 
+function aladin(magicalSource, distance, n) { 
+  let currentSource = magicalSource[0];
+  let lowestIndex = 0;
+  for (let index = 0; index < n; index++) {
+   
+    if (currentSource >= distance[index]) {
+      let nextMagicalSource = magicalSource[index + 1] ? magicalSource[index + 1] : 0;
+      currentSource = magicalSource[index] - distance[index] + nextMagicalSource;
+      
+    } else { 
+      if (index === n-1 && magicalSource[index]<distance[index]) return -1;
+      currentSource = magicalSource[index];
+      lowestIndex = index;
+      continue;
+    }
+  }
+  return lowestIndex;
 
 }
 module.exports = aladin;
